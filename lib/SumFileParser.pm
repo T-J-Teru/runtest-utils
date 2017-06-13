f44f4f53ba5cbecf6dfdbfe083f12cd761a05e9e
Andrew Burgess <andrew.burgess@embecosm.com>
2017-06-13 11:19:50 +0100

    Add sum4way script

---
+++ lib/SumFileParser.pm
@@ -0,0 +1,92 @@
+package SumFileParser;
+
+use strict;
+use warnings;
+no indirect;
+no autovivification;
+
+use Carp;
+use Carp::Assert;
+use TestResult;
+
+=pod
+
+=head1 NAME
+
+SumFileParser - Parse DeJaGNU test summary files.
+
+=head1 SYNOPSIS
+
+  Quick code sample should go here...
+
+=head1 METHODS
+
+The methods for this module are listed here:
+
+=over 4
+
+=cut
+
+#============================================================================#
+
+=pod
+
+=item I<Public>: B<parse>
+
+A function to parse the contents of a summary file, returns a list of
+TestResult objects.
+
+=cut
+
+sub parse {
+  my $filename = shift;
+
+  my @results;
+  open my $in, $filename
+    or croak ("Failed to open '$filename': $!");
+
+  while (<$in>)
+  {
+    if (m/^(UNSUPPORTED|FAIL|PASS|XFAIL|XPASS|UNRESOLVED|KFAIL|KPASS|UNTESTED): /)
+    {
+      my $status = $1;
+      $_ =~ s/^[^:]+:\s*//;
+
+      $_ =~ m/(^[^:]+):\s*(.*)$/;
+      my $testname = $2;
+
+      my $rev = reverse $1;
+      $rev =~ m#^([^/]+)/(.*)#;
+      my $file = reverse ($1);
+      my $dir = reverse ($2);
+
+      my $test = TestResult->new (-directory => $dir,
+                                  -filename => $file,
+                                  -testname => $testname,
+                                  -status => $status);
+      defined $test or die;
+      push @results, $test;
+    }
+  }
+
+  close $in
+    or croak ("Failed to close '$filename': $!");
+
+  return @results;
+}
+
+#============================================================================#
+
+=pod
+
+=back
+
+=head1 AUTHOR
+
+Andrew Burgess, 20 May 2016
+
+=cut
+
+#============================================================================#
+#Return value of true so that this file can be used as a module.
+1;
---
+++ lib/TestResult.pm
@@ -0,0 +1,218 @@
+package TestResult;
+
+use strict;
+use warnings;
+no indirect;
+no autovivification;
+
+=pod
+
+=head1 NAME
+
+TestResult - A single test results from a DeJaGNU summary file.
+
+=head1 SYNOPSIS
+
+  Quick code sample should go here...
+
+=head1 METHODS
+
+The methods for this module are listed here:
+
+=over 4
+
+=cut
+
+#============================================================================#
+
+=pod
+
+=item I<Public>: B<is_pass>
+
+Return true if this test should be considered a passing test.  Currently
+this covers PASS, XPASS, KPASS.
+
+=cut
+
+sub is_pass {
+  my $self = shift;
+  my $status = $self->get_status ();
+  return (($status eq "PASS")
+            or ($status eq "XPASS")
+            or ($status eq "KPASS"));
+}
+
+#============================================================================#
+
+=pod
+
+=item I<Public>: B<is_bad>
+
+Return true if this test result is bad, so FAIL, UNRESOLVED, UNTESTED.
+
+=cut
+
+sub is_bad {
+  my $self = shift;
+  my $status = $self->get_status ();
+  return (($status eq "FAIL")
+            or ($status eq "UNTESTED")
+            or ($status eq "UNRESOLVED"));
+}
+
+#========================================================================#
+
+=pod
+
+=item I<Public>: B<get_status>
+
+Currently undocumented.
+
+=cut
+
+sub get_status {
+  my $self = shift;
+  return $self->{ __status__ };
+}
+
+#========================================================================#
+
+=pod
+
+=item I<Public>: B<get_testname>
+
+Currently undocumented.
+
+=cut
+
+sub get_testname {
+  my $self = shift;
+
+  # Return cached testname if it exists.
+  return $self->{ __testname__ } if (exists $self->{ __testname__ });
+
+  # Strip additional messages from the testname.
+  my $testname = $self->{ __original_testname__ };
+  $testname =~ s/\((?:first|second|third) time\)\s*//;
+  $testname =~ s/\(timeout\)\s*//;
+  $testname =~ s/struct_param_single_reg_loc"/struct_param_single_reg_loc/;
+  $testname =~ s/\(PRMS:? [^)]+\)\s*//;
+  $testname =~ s#(Couldn't compile) .*(gdb/testsuite/gdb.java/jnpe.java)#$1 $2#;
+  $testname =~ s#(Couldn't compile) .*(gdb/testsuite/gdb.[^/]+/.*)#$1 $2#;
+  $testname =~ s#\(open '.*(gdb/testsuite/gdb.[^/']+/[^']+)'\)#(open '$1')#;
+  $testname =~ s#print characters#print elements#;
+  $testname =~ s#with characters set to#with elements set to#;
+
+  $testname =~ s/\s*$//;
+
+  # Cache and return the testname
+  $self->{ __testname__ } = $testname;
+  return $self->{ __testname__ };
+}
+
+#========================================================================#
+
+=pod
+
+=item I<Public>: B<get_filename>
+
+Currently undocumented.
+
+=cut
+
+sub get_filename {
+  my $self = shift;
+  return $self->{ __filename__ };
+}
+
+#========================================================================#
+
+=pod
+
+=item I<Public>: B<get_directory>
+
+Currently undocumented.
+
+=cut
+
+sub get_directory {
+  my $self = shift;
+  return $self->{ __directory__ };
+}
+
+#========================================================================#
+
+=pod
+
+=item I<Public>: B<get_id>
+
+Currently undocumented.
+
+=cut
+
+sub get_id {
+  my $self = shift;
+  return $self->get_directory () ."/".
+    $self->get_filename () .": ".
+    $self->get_testname ();
+}
+
+#========================================================================#
+
+=pod
+
+=item I<Public>: B<new>
+
+Create a new instance of TestResult and then call initialise
+on it.
+
+=cut
+
+sub new {
+  my $class = shift;
+
+  #-----------------------------#
+  # Don't change this method    #
+  # Change 'initialise' instead #
+  #-----------------------------#
+
+  my $self  = bless {}, $class;
+  $self->initialise(@_);
+  return $self;
+}
+
+#============================================================================#
+
+=pod
+
+=item I<Private>: B<initialise>
+
+Initialise this instance of this class.
+
+=cut
+
+sub initialise {
+  my $self = shift;
+  my %args = @_;
+
+  $self->{ __directory__ } = $args{ -directory };
+  $self->{ __filename__ } = $args{ -filename };
+  $self->{ __original_testname__ } = $args{ -testname };
+  $self->{ __status__ } = $args{ -status };
+}
+
+#============================================================================#
+
+=pod
+
+=back
+
+=head1 AUTHOR
+
+Andrew Burgess, 20 May 2016
+
+=cut
+
+#============================================================================#
+#Return value of true so that this file can be used as a module.
+1;

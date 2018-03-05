ea0e0f65ff9ecc805e6d7d6fde795cface98177a
Andrew Burgess <andrew.burgess@embecosm.com>
2018-03-05 16:49:14 +0000

    libs: Improve SumFileParser
    Ran into some .sum files that didn't have any 'Running' lines at all, I
    needed to tweak the SumFileParser to allow it to parse these files.
    
    The idea now is that if we see a test status before a 'Running' line,
    then we expect to never see a 'Running' line.  If we see a test status
    without an exp filename prefix, then we expect to have seen a 'Running'
    line.

--- lib/SumFileParser.pm
+++ lib/SumFileParser.pm
@@ -8,6 +8,7 @@ no autovivification;
 use Carp;
 use Carp::Assert;
 use TestResult;
+use boolean;
 
 =pod
 
@@ -36,6 +37,31 @@ The methods for this module are listed here:
 
 =pod
 
+=item I<Private>: B<split_full_exp_file_name>
+
+Static function that takes a single parameter, the full name of an exp
+file, and returns a list of two elements, all parts of the full name upto
+the actual filename, and the actual filename.
+
+So, 'a/b/c.exp' will return ('a/b', 'c.exp').
+
+=cut
+
+sub split_full_exp_file_name {
+  my $fullname = shift;
+
+  my $rev = reverse ($fullname);
+  $rev =~ m#^([^/]+)/(.*)#;
+  my $file = reverse ($1);
+  my $dir = reverse ($2);
+
+  return ($dir, $file);
+}
+
+#========================================================================#
+
+=pod
+
 =item I<Public>: B<parse>
 
 A function to parse the contents of a summary file, returns a list of
@@ -53,47 +79,99 @@ sub parse {
   my $expfile = { __full_name__ => undef,
                   __file__ => undef,
                   __dir__ => undef };
+  my $using_running_lines = true;
 
   while (<$in>)
   {
+    # Some testsuites (like GCC) don't prefix all test results with the
+    # name of the exp file being run, instead, we must pull the name from
+    # the previous Running line.
     if (m#^Running .*/testsuite/(.*) \.\.\.$#)
     {
-      $expfile->{ __full_name__ } = $1;
+      # Some testsuites, such as GDB when run in parallel, don't produce
+      # Running lines at all, and we should grab the name of the exp file
+      # from the prefix on each test status line.  However, if we get into
+      # a state where we're mixing these two models then things are going
+      # to go wrong.  If we saw our first test status line before we saw a
+      # 'Running' line, then we don't expect to ever see a running line.
+      if (not ($using_running_lines))
+      {
+        print "Line: $_";
+        croak "Found a 'Running' line after a test status line";
+      }
 
-      my $rev = reverse ($expfile->{ __full_name__ });
-      $rev =~ m#^([^/]+)/(.*)#;
-      $expfile->{ __file__ } = reverse ($1);
-      $expfile->{ __dir__ } = reverse ($2);
+      $expfile->{ __full_name__ } = $1;
+      ($expfile->{ __dir__ }, $expfile->{ __file__ })
+        = split_full_exp_file_name ($expfile->{ __full_name__ });
     }
 
+    # This is a test status line.
     if (m/^(UNSUPPORTED|FAIL|PASS|XFAIL|XPASS|UNRESOLVED|KFAIL|KPASS|UNTESTED): /)
     {
       my $status = $1;
+
+      # Discard the status from the front of the line.
       $_ =~ s/^[^:]+:\s*//;
 
       my ($file, $dir, $testname);
+
+      # Now if it looks like this line has a exp filename prefix then we
+      # can extract this, and possibly cross-check it with the previous
+      # 'Running' line, or, if there was no previous 'Running' line, this
+      # will provide the name of the exp file that produced this test.
       if ($_ =~ m/(^[^: ]+):\s*(.*)$/)
       {
-        my ($curr_expfile, $rev);
+        my $curr_expfile;
         ($curr_expfile, $testname) = ($1, $2);
 
-        if ($curr_expfile ne $expfile->{ __full_name__ })
+        if (not (defined ($expfile->{__full_name__})))
+        {
+          # We've found a status result before an 'Running' line.  We allow
+          # this so long as we never seen any Running lines.  Some '.sum'
+          # files don't seem to have the 'Running' lines at all.
+          $using_running_lines = false;
+
+          # Use file and directory name from the line.
+          ($dir, $file) = split_full_exp_file_name ($curr_expfile);
+        }
+        elsif ($curr_expfile ne $expfile->{ __full_name__ })
         {
           print "Line: $_";
           croak ("expected a test in '".$expfile->{ __full_name__ }
                    ."' but found one in '".$curr_expfile."'");
         }
+        else
+        {
+          $file = $expfile->{__file__};
+          $dir = $expfile->{__dir__};
+        }
       }
+      # There was no exp filename prefix, we rely on their having been a
+      # previous 'Running' line we can get the name of the exp file from.
       else
       {
         $testname = $_;
         chomp $testname;
+
+        if (not (defined ($expfile->{__full_name__})))
+        {
+          print "Line: $_";
+          croak ("no previous Running line found before test without ".
+                   "test file prefix");
+        }
+        $file = $expfile->{__file__};
+        $dir = $expfile->{__dir__};
       }
 
+      # Check everything is valid.
       assert (defined ($testname));
+      assert (defined ($status));
+      assert (defined ($dir));
+      assert (defined ($file));
 
-      my $test = TestResult->new (-directory => $expfile->{ __dir__ },
-                                  -filename => $expfile->{ __file__ },
+      # Create the new result.
+      my $test = TestResult->new (-directory => $dir,
+                                  -filename => $file,
                                   -testname => $testname,
                                   -status => $status);
       defined $test or die;
